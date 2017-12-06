ded40897c9d24593331dd7cadce55371b256f8d9
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2017-12-06 13:23:54 -0700

    Back to welcome screen

--- app/src/main/java/com/example/nathaniel/heyteacher/MainActivity.java
+++ app/src/main/java/com/example/nathaniel/heyteacher/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.nathaniel.heyteacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createRoom(View view){
+        Toast.makeText(this, "Create room", Toast.LENGTH_SHORT).show();
+    }
+
+    public void joinRoom(View view){
+        Toast.makeText(this, "Join Room", Toast.LENGTH_SHORT).show();
     }
 }
