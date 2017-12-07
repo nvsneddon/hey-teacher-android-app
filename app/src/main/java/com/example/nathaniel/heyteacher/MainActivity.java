ca7c16db1044d68e1cffc0d406da61eece575e31
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2017-12-06 17:02:11 -0700

    Working room code generator

--- app/src/main/java/com/example/nathaniel/heyteacher/MainActivity.java
+++ app/src/main/java/com/example/nathaniel/heyteacher/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.nathaniel.heyteacher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nameField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameField = (EditText) findViewById(R.id.editText);
     }
 
     public void createRoom(View view){
-        Toast.makeText(this, "Create room", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Create room", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, TeacherScreen.class);
+        intent.putExtra("name", nameField.getText());
+        startActivity(intent);
     }
 
     public void joinRoom(View view){
--- app/src/main/java/com/example/nathaniel/heyteacher/TeacherScreen.java
+++ app/src/main/java/com/example/nathaniel/heyteacher/TeacherScreen.java
@@ -2,6 +2,7 @@ package com.example.nathaniel.heyteacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.net.URISyntaxException;
 import java.util.Random;
@@ -15,6 +16,7 @@ import org.json.JSONObject;
 public class TeacherScreen extends AppCompatActivity {
 
     private int roomNumber;
+    private Socket socket;
 
     public boolean checkNumberAvailability(int roomNr){
         return true;
@@ -25,10 +27,37 @@ public class TeacherScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_screen);
+
+        try {
+            socket = IO.socket("http://54.148.39.15:8080");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        socket.connect();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if(socket.connected()){
+            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+        } else{
+            Toast.makeText(this, "Not Connected", Toast.LENGTH_SHORT).show();
+        }
         do {
             Random rand = new Random();
             roomNumber = rand.nextInt(10000);
         } while(!checkNumberAvailability(roomNumber));
 
+        socket.emit("room", roomNumber);
+    }
+
+    @Override
+    protected void onDestroy(){
+        //insert disconnecting code here
+        socket.disconnect();
+        super.onDestroy();
     }
 }
